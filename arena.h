4b6f03432415a9aec62eb9ab090e8c0cb027ef3a
Larry Reaves <larry@yrral.net>
2014-02-01 16:30:46 -0500

    fix type, further cleanup in arena.h

--- arena.h
+++ arena.h
@@ -1,9 +1,11 @@
 #ifndef __ARENA_H__
 #define __ARENA_H__
 
-// specified in millimeters
+const static int OBSTACLE_COUNT = 6;
+
 // ARENA_WIDTH and ARENA_HEIGHT must be defines
 // to simplify buffer allocation
+// all distances in this file are specified in millimeters
 
 /*
 // real arena
@@ -14,7 +16,6 @@ const static int START_END = 1500;
 // we don't mine early
 const static  int MINE_BEGIN = 4440;
 */
-const static int OBSTACLE_COUNT = 6;
 
 /*
 // lab arena
@@ -24,9 +25,10 @@ const static int START_END = 500;
 const static  int MINE_BEGIN = 1250;
 */
 
+
 // home arena
 #define ARENA_WIDTH 1765
-#define ARENA_HIEGHT 800
+#define ARENA_HEIGHT 800
 const static int START_END = 500;
 const static  int MINE_BEGIN = 1250;
 
