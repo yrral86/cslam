7bbcadc42d8c23a398371a173bdd01ceb5e43172
Larry Reaves <larry@yrral.net>
2014-02-01 16:09:04 -0500

    clarify arena definition

--- arena.h
+++ arena.h
@@ -1,15 +1,33 @@
 #ifndef __ARENA_H__
 #define __ARENA_H__
 
-#define ARENA_WIDTH 1765
-#define ARENA_HEIGHT 800
+// specified in millimeters
+// ARENA_WIDTH and ARENA_HEIGHT must be defines
+// to simplify buffer allocation
 
-//const static int ARENA_WIDTH = 7380;
-//const static int ARENA_WIDTH = 1765;
-//const static int ARENA_HEIGHT = 3880;
-//const static int ARENA_HEIGHT = 800;
-const static int START_END = 150;
-const static  int MINE_BEGIN = 444;
+/*
+// real arena
+#define ARENA_WIDTH 7380
+#define ARENA_HIEGHT 3880
+const static int START_END = 1500;
+// TODO we may want to add some tolerance to MINE_BEGIN so we make sure
+// we don't mine early
+const static  int MINE_BEGIN = 4440;
+*/
 const static int OBSTACLE_COUNT = 6;
+
+/*
+// lab arena
+#define ARENA_WIDTH 3250
+#define ARENA_HEIGHT 1700
+const static int START_END = 500;
+const static  int MINE_BEGIN = 1250;
+*/
+
+// home arena
+#define ARENA_WIDTH 1765
+#define ARENA_HIEGHT 800
+const static int START_END = 500;
+const static  int MINE_BEGIN = 1250;
 
 #endif
