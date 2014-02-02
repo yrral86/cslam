6ef69207f7bde6d9b8533a0a0fe06b650714451f
Larry Reaves <larry@yrral.net>
2014-02-01 19:35:28 -0500

    remove excess buffers for performance

--- slam.c
+++ slam.c
@@ -10,17 +10,16 @@ const static int INITIAL_ANGLE_VARIANCE = 15;
 const static int BUFFER_WIDTH = ARENA_WIDTH/2;
 const static int BUFFER_HEIGHT = ARENA_HEIGHT/2;
 const  static int BUFFER_SIZE = ARENA_HEIGHT*ARENA_WIDTH/4;
-// first 2 buffers are history, so you only have
-// BUFFER_COUNT - 2 to work with
+// 2, one for current, one for history
 #define BUFFER_HISTORY 2
-#define BUFFER_COUNT 2000
-static uint8_t* map[BUFFER_COUNT];
+#define PARTICLE_COUNT 200
+static uint8_t* map[BUFFER_HISTORY];
 // first particle is reserved for current position
 // (relative to buffer center, 0 degrees = + x)
 // second particle is reserved for initializing the top 10% indice list
-static particle particles[BUFFER_COUNT];
+static particle particles[PARTICLE_COUNT];
 // keep the indices of the top 10 %
-int top_ten[(BUFFER_COUNT - BUFFER_HISTORY)/10];
+int top_ten[PARTICLE_COUNT/10];
 
 static double spacing;
 static uint64_t last_poll;
@@ -33,7 +32,7 @@ int main (int argc, char **argv) {
   scans = malloc(sample_count*sizeof(raw_sensor_scan));
 
   // allocate buffers
-  for (i = 0; i < BUFFER_COUNT; i++)
+  for (i = 0; i < BUFFER_HISTORY; i++)
     map[i] = malloc(sizeof(uint8_t)*BUFFER_SIZE);
 
   spacing = 240.0/RAW_SENSOR_DISTANCES;
@@ -50,23 +49,15 @@ int main (int argc, char **argv) {
 
   rand_normal_init();
 
-  // initialize variance
-  particles[0].x = INITIAL_POSITION_VARIANCE;
-  particles[0].y = INITIAL_POSITION_VARIANCE;
-  particles[0].theta = INITIAL_ANGLE_VARIANCE;
-
   // initialize terrible score
-  particles[1] = particle_init(0,0,0);
+  particles[1] = particle_init(0, 0, 0);
   particle_add_sample(particles + 1, 100000000000);
 
   iterations = 0;
 
   while(1) {
-    for (i = BUFFER_HISTORY; i < BUFFER_COUNT; i++) {
-      // reset buffers
-      bzero(map[i], BUFFER_SIZE*sizeof(uint8_t));
-
-      // generate particles
+    // generate particles
+    for (i = 0; i < PARTICLE_COUNT; i++) {
       // generate random position and angle varation
       particles[i] = particle_init(rand_normal(INITIAL_POSITION_VARIANCE),
 				   rand_normal(INITIAL_POSITION_VARIANCE),
@@ -79,6 +70,7 @@ int main (int argc, char **argv) {
       usleep(sleep_time);
     else printf("sleepless\n");
 
+    // get samples
     for (j = 0; j < sample_count; j++) {
       scans[j] = sensor_read_raw();
       last_poll = utime();
@@ -91,21 +83,17 @@ int main (int argc, char **argv) {
       }
     }
 
-    // filter the candidates in a loop, testing a few new
-    // directions each time for each particle and dropping 
-    // any particles that have too much new information
+    // score particles by how much new information they contain
     uint8_t *buffer;
     particle p;
     int filtered, min_index;
-    int count = BUFFER_COUNT - BUFFER_HISTORY;
     int last_top_ten, current_top_ten, other_top_ten, x, y, difference;
     double distance, degrees, theta, dx, dy;
 
     // evaulate each direction for each particle
-    for (i = BUFFER_HISTORY; i < count + BUFFER_HISTORY; i++) {
+    for (i = 0; i < PARTICLE_COUNT; i++) {
       filtered = 0;
-      buffer = map[i];
-      p = particle_init(particles[i].x, particles[i].y, particles[i].theta);
+      p = particles[i];
 
       for (j = 0; j < RAW_SENSOR_DISTANCES; j++) {
 	for (k = 0; k < sample_count; k++) {
@@ -123,8 +111,9 @@ int main (int argc, char **argv) {
     
 	  if (in_bounds(x, y)) {
 	    l = index_from_x_y(x, y);
-	    difference = map[i][l] - map[1][l];
-	    if (difference > 50)
+	    difference = 255 - map[1][l];
+	    // allow roughly 18 attenuations from full recognition
+	    if (difference > 200)
 	      filtered++;
 	  } else filtered++;
 	}
@@ -134,14 +123,14 @@ int main (int argc, char **argv) {
 
     // find best particle
     // finds top 10%, ordered best first
-    last_top_ten = count/10 - 1;
+    last_top_ten = PARTICLE_COUNT/10 - 1;
 
-    // initialize top_ten to point to terrible score
-    bzero(top_ten, sizeof(int)*count/10);
+    // initialize top_ten to point to terrible score (particles[1])
+    bzero(top_ten, sizeof(int)*PARTICLE_COUNT/10);
     for (i = 0; i <= last_top_ten; i++)
       top_ten[i]++;
     
-    for (i = BUFFER_HISTORY; i < count - BUFFER_HISTORY; i ++) {
+    for (i = 0; i < PARTICLE_COUNT; i++) {
       p = particles[i];
       current_top_ten = last_top_ten;
 
@@ -150,7 +139,7 @@ int main (int argc, char **argv) {
 	top_ten[current_top_ten] = i;
 
       // bubble towards front of list
-      while (current_top_ten > 0 && particles[top_ten[current_top_ten - 1]].score < particles[top_ten[current_top_ten]].score) {
+      while (current_top_ten > 0 && p.score < particles[top_ten[current_top_ten - 1]].score) {
 	other_top_ten = top_ten[current_top_ten];
 	top_ten[current_top_ten] = top_ten[current_top_ten - 1];
 	top_ten[current_top_ten - 1] = other_top_ten;
@@ -167,33 +156,35 @@ int main (int argc, char **argv) {
 
     // attenuate map
     for (i = 0; i < BUFFER_SIZE; i++)
-	map[0][i] *= 0.85;
+	map[0][i] *= 0.75;
 
     // update localization
+    // assuming 10% momentum
     p = particles[min_index];
-    particles[0].x += p.x;
-    particles[0].y += p.y;
-    particles[0].theta += p.theta;
+    particles[0].x += 1.1*p.x;
+    particles[0].y += 1.1*p.y;
+    particles[0].theta += 1.1*p.theta;
 
     // draw position
     for (i = -5; i < 6; i++)
-      for (j = -5; j < 6; j++ )
-	record_map_position(0, ARENA_WIDTH/4 + p.x + i,
-			    ARENA_HEIGHT/4 + p.y + j, 255);
+      for (j = -5; j < 6; j++ ) {
+	record_map_position(0, BUFFER_WIDTH/2 + p.x + i,
+			    BUFFER_HEIGHT/2 + p.y + j, 255);
+	record_map_position(1, BUFFER_WIDTH/2 + p.x + i,
+			    BUFFER_HEIGHT/2 + p.y + j, 255);
+      }
 
     // draw
     display();
 
     // clear position
     for (i = -5; i < 6; i++)
-      for (j = -5; j < 6; j++ )
-	record_map_position(0, ARENA_WIDTH/4 + p.x + i,
-			    ARENA_HEIGHT/4 + p.y + j, 0);
-
-    // copy best into current map
-    for (i = 0; i < BUFFER_SIZE; i++)
-      if (map[min_index][i] == 255)
-	map[0][i] = 255;
+      for (j = -5; j < 6; j++ ) {
+	record_map_position(0, BUFFER_WIDTH/2 + p.x + i,
+			    BUFFER_HEIGHT/2 + p.y + j, 0);
+	record_map_position(1, BUFFER_WIDTH/2 + p.x + i,
+			    BUFFER_HEIGHT/2 + p.y + j, 0);
+      }
 
     glutMainLoopEvent();
 
@@ -201,15 +192,14 @@ int main (int argc, char **argv) {
 
     // copy current map into historical map
     // and attenuate
-    if (iterations % 20)
-      for (i = 0; i < BUFFER_SIZE; i++) {
-	map[1][i] *= 0.99;
-	if (map[0][i] > 200)
-	  if (map[1][i] < 205)
-	    map[1][i] += 50;
-	  else
-	    map[1][i] = 255;
-      }
+    for (i = 0; i < BUFFER_SIZE; i++) {
+      map[1][i] *= 0.99;
+      if (map[0][i] > 150)
+	if (map[1][i] < 235)
+	  map[1][i] += 50;
+	else
+	  map[1][i] = 255;
+    }
   }
 
   return 0;
