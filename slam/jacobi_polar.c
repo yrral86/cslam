72a5e5f53ff418ba6f8670045bcd87c2f340062b
Larry Reaves <larry@yrral.net>
2014-02-27 21:34:11 -0500

    add jacobi_polar.c

---
+++ slam/jacobi_polar.c
@@ -0,0 +1,9 @@
+#include "jacobi_polar"
+
+void jacobian_polar(particle p) {
+  int i, d;
+  // TODO: _ETH
+  for (i = 0; i < RAW_SENSOR_DISTANCES_USB; i++) {
+    d = landmark_tree_node_find_distance(p.map, i);
+  }
+}
