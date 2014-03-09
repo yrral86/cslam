550458eed15fceac8d937cfd5e6840b5ff008fba
Larry Reaves <larry@yrral.net>
2014-03-09 18:49:35 -0400

    fix jacobi problems

--- slam/jacobi_polar.c
+++ slam/jacobi_polar.c
@@ -1,9 +1,9 @@
-#include "jacobi_polar"
+#include "jacobi_polar.h"
 
 void jacobian_polar(particle p) {
   int i, d;
   // TODO: _ETH
   for (i = 0; i < RAW_SENSOR_DISTANCES_USB; i++) {
-    d = landmark_map_find_distance(p.map, i);
+    d = landmark_map_find_distance(p.map, i, p);
   }
 }
--- slam/jacobi_polar.h
+++ slam/jacobi_polar.h
@@ -1,6 +1,8 @@
 #ifndef __JACOBI_POLAR__
 #define __JACOBI_POLAR__
 
+#include "particle.h"
+
 void jacobian_polar(particle);
 
 #endif
