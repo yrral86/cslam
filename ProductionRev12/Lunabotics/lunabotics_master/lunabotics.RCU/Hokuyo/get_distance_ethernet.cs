075cd0c768b3195449bb22a8b70df3b99ff09b6a
mgrubb1 <mgrubb1@mix.wvu.edu>
2014-03-09 17:29:14 -0400

    Missing Hokuyo folder from previos commit

---
+++ ProductionRev12/Lunabotics/lunabotics_master/lunabotics.RCU/Hokuyo/get_distance_ethernet.cs
@@ -0,0 +1,135 @@
+﻿/*!
+ * \file
+ * \brief Get distance data from Ethernet type URG
+ * \author Jun Fujimoto
+ * $Id: get_distance_ethernet.cs 403 2013-07-11 05:24:12Z fujimoto $
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using SCIP_library;
+
+namespace lunabotics.RCU.Hokuyo
+{
+
+    class get_distance_ethernet
+    {
+
+        public List<long> EthernetScan()
+        {
+            const int start_step = 0;
+            const int end_step = 760;
+
+            //List to hold all 760 distance values
+            List<long> distances = new List<long>();
+
+            try
+            {
+
+                string ip_address = "192.168.0.10";
+                int port_number = 10940;
+
+                TcpClient urg = new TcpClient();
+                urg.Connect(ip_address, port_number);
+                NetworkStream stream = urg.GetStream();
+
+                write(stream, SCIP_Writer.SCIP2());
+                read_line(stream);  // ignore echo back
+                write(stream, SCIP_Writer.MD(start_step, end_step));
+                read_line(stream);  // ignore echo back
+
+                long time_stamp = 0;
+
+                string receive_data = read_line(stream);
+                if (!SCIP_Reader.MD(receive_data, ref time_stamp, ref distances))
+                {
+                    Console.WriteLine(receive_data);
+                }
+                else
+                {
+                    if (distances.Count == 0)
+                    {
+                        Console.WriteLine(receive_data);
+                    }
+                    //// show distance data
+                    //for (int j = 0; j < 760; j++)
+                    //{
+                    //    Console.WriteLine(distances[j].ToString());
+                    //}
+                }
+
+                write(stream, SCIP_Writer.QT());    // stop measurement mode
+                read_line(stream); // ignore echo back
+                stream.Close();
+                urg.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return distances;
+        }
+
+
+        /// <summary>
+        /// Read to "\n\n" from NetworkStream
+        /// </summary>
+        private string read_line(NetworkStream stream)
+        {
+            if (stream.CanRead)
+            {
+                StringBuilder sb = new StringBuilder();
+                bool is_NL2 = false;
+                bool is_NL = false;
+                do
+                {
+                    char buf = (char)stream.ReadByte();
+                    if (buf == '\n')
+                    {
+                        if (is_NL)
+                        {
+                            is_NL2 = true;
+                        }
+                        else
+                        {
+                            is_NL = true;
+                        }
+                    }
+                    else
+                    {
+                        is_NL = false;
+                    }
+                    sb.Append(buf);
+                } while (!is_NL2);
+
+                return Convert.ToString(sb);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// write data
+        /// </summary>
+        private bool write(NetworkStream stream, string data)
+        {
+            if (stream.CanWrite)
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                stream.Write(buffer, 0, buffer.Length);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProductionRev12/Lunabotics/lunabotics_master/lunabotics.RCU/Hokuyo/get_distance_serial.cs
@@ -0,0 +1,81 @@
+﻿/*!
+ * \file
+ * \brief Get distance data from serial type URG
+ * \author Jun Fujimoto
+ * $Id: get_distance_serial.cs 403 2013-07-11 05:24:12Z fujimoto $
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using SCIP_library;
+
+class get_distance_serial
+{
+    static void Main(string[] args)
+    {
+        const int GET_NUM = 10;
+        const int start_step = 0;
+        const int end_step = 760;
+        try {
+            string port_name;
+            int baudrate;
+            get_serial_information(out port_name, out baudrate);
+
+            SerialPort urg = new SerialPort(port_name, baudrate);
+            urg.NewLine = "\n\n";
+
+            urg.Open();
+
+            urg.Write(SCIP_Writer.SCIP2());
+            urg.ReadLine(); // ignore echo back
+            urg.Write(SCIP_Writer.MD(start_step, end_step));
+            urg.ReadLine(); // ignore echo back
+
+            List<long> distances = new List<long>();
+            long time_stamp = 0;
+            for (int i = 0; i < GET_NUM; ++i) {
+                string receive_data = urg.ReadLine();
+                if (!SCIP_Reader.MD(receive_data, ref time_stamp, ref distances)) {
+                    Console.WriteLine(receive_data);
+                    break;
+                }
+                if (distances.Count == 0) {
+                    Console.WriteLine(receive_data);
+                    continue;
+                }
+                // show distance data
+                Console.WriteLine("time stamp: " + time_stamp.ToString() + " distance[100] : " + distances[100].ToString());
+            }
+
+            urg.Write(SCIP_Writer.QT()); // stop measurement mode
+            urg.ReadLine(); // ignore echo back
+            urg.Close();
+        } catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        } finally {
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// get connection information from user.
+    /// </summary>
+    private static void get_serial_information(out string port_name, out int baudrate)
+    {
+        port_name = "COM3";
+        baudrate = 115200;
+        Console.WriteLine("Please enter port name. [default: " + port_name + "]");
+        string str = Console.ReadLine();
+        if (str != "") {
+            port_name = str;
+        }
+        Console.WriteLine("Please enter baudrate. [default: " + baudrate.ToString() + "]");
+        str = Console.ReadLine();
+        if (str != "") {
+            baudrate = int.Parse(str);
+        }
+        Console.WriteLine("Connect setting = Port name : " + port_name + " Baudrate : " + baudrate.ToString());
+    }
+}
