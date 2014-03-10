d3b41d032aed211329c724d9cf3428fa0686db48
Larry Reaves <larry@yrral.net>
2014-03-10 03:46:22 -0400

    another test

--- slam/Slam/SlamTestClass.cs
+++ slam/Slam/SlamTestClass.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Slam
 {
@@ -38,6 +39,41 @@ namespace Slam
 			Assert.IsTrue (Swarm.swarm_get_best_y () - 3880 / 4 < 5);
 			// within 5 degrees
 			Assert.IsTrue (Swarm.swarm_get_best_theta () - 0 < 5);
+		}
+
+		[Test ()]
+		public void Test04Path ()
+		{
+			int x, y, dx, dy;
+			int[] distances;
+			distances = new int[3];
+			x = 1500 / 2;
+			y = 3880 / 4;
+			dx = 10;
+			dy = 10;
+			Swarm.swarm_init (3, 180, 7380, 3880, 1500);
+			Swarm.swarm_move (0, 0, 0);
+			// left
+			distances [0] = 3 * y;
+			// forward
+			distances [1] = 7380 - x;
+			//right
+			distances [2] = y;
+			Swarm.swarm_update (ref distances);
+			// within 5 mm
+			Assert.IsTrue (Swarm.swarm_get_best_x () - x < 5);
+			Assert.IsTrue (Swarm.swarm_get_best_y () - y < 5);
+			//for (int i = 0; i < 10; i++) {
+				Swarm.swarm_move (dx, dy, 0);
+				distances [0] -= dy;
+				distances [1] -= dx;
+				distances [2] += dy;
+				Swarm.swarm_update (ref distances);
+				x += dx;
+				y += dy;
+				Assert.IsTrue (Swarm.swarm_get_best_x () - x < 5);
+				Assert.IsTrue (Swarm.swarm_get_best_y () - y < 5);
+			//}
 		}
 	}
 }
